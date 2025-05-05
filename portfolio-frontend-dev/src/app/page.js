aa8994a5f54ae07019429df9bf9a1ca0f3dc6380
Sergi López Hernández <serkio.lh@gmail.com>
2025-05-05 02:55:44 +0200

    Main

--- portfolio-frontend-dev/src/app/page.js
+++ portfolio-frontend-dev/src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Header from "./components/Header";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 // React Server Components
 import { motion, useScroll } from "framer-motion";
 const Home = () => {
@@ -13,12 +14,36 @@ const Home = () => {
 				style={{ scaleX: scrollYProgress }}
 			>
 				<h1 className="text-white text-8xl text-center">
-					Sergi López <br />
-					Hernández
+					<span className="text-transparent bg-linear-to-r from-purple-500 to-blue-600 bg-clip-text">
+						Frontend
+					</span>{" "}
+					<br />
+					Developer
 				</h1>
-				<p className="text-[#8716f7] text-transparent bg-clip-text bg-linear-65 from-purple-500 to-blue-800 text-center text-4xl mt-4 ">
-					Frontend Developer
+				<p className="text-[#8716f7] text-transparent bg-clip-text bg-linear-to-l from-purple-500 to-blue-600 text-center text-4xl mt-4 ">
+					Sergi López Hernández
 				</p>
+				<p className="font-[Arial] text-[1.7rem] p-5 text-white text-transparent bg-clip-text bg-linear-65 from-purple-500 to-blue-800 text-center text-4xl mt-4 ">
+					<TypeAnimation
+						sequence={[
+							"Web developer",
+							1000,
+							"Web developer with a passion for creating",
+							1000,
+							"Web developer with a passion for creating beautiful and responsive websites.",
+							3000,
+						]}
+						wrapper="span"
+						speed={55}
+						repeat={Infinity}
+					/>
+				</p>
+				<button
+					type="button"
+					className="flex border-[#c300ff] mx-auto mt-15 rounded-full border-3 p-2 shadow-lg  shadow-[#c300ff] hover:shadow-[#4400ff] hover:border-[#4400ff]  transition duration-500 text-white items-center justify-center text-center cursor-pointer"
+				>
+					View Projects
+				</button>
 				<div className="flex justify-center items-center ">
 					<Image
 						src="/Logo_Gradient-removebg-preview.png"
