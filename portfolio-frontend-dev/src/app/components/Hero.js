e6c43247da07cb0082cfc5850e7e2d3eba936281
Sergi López Hernández <serkio.lh@gmail.com>
2025-05-09 16:56:01 +0200

    Logo en Hero

--- portfolio-frontend-dev/src/app/components/Hero.js
+++ portfolio-frontend-dev/src/app/components/Hero.js
@@ -11,9 +11,17 @@ const Hero = () => {
 	return (
 		<>
 			<section id="home" className="dark h-[100vh]">
-				<div className=" h-screen scroll-smooth bg-cover bg-end bg-repeat-space flex flex-col items-center justify-center p-10 bg-gradient-to-b from-white to-purple-300">
-					<h1 className="text-white md:text-9xl text-6xl transition-all duration-500  text-center">
-						<span className="bg-linear-to-r from-purple-500 to-blue-600 bg-clip-text dark:text-white  text-transparent ">
+				<div className=" h-screen scroll-smooth  flex flex-col items-center justify-start p-10 bg-gradient-to-b from-white to-purple-300">
+					<div className="flex justify-center items-center mt-2 ">
+						<Image
+							src="/descargar (13) (1) (1).png"
+							alt="logo"
+							width={400}
+							height={400}
+						/>
+					</div>
+					<h1 className="text-white md:text-9xl text-6xl  transition-all duration-500  text-center">
+						<span className="bg-linear-to-r from-purple-500 to-blue-600 bg-clip-text   text-transparent ">
 							Frontend
 						</span>{" "}
 						<br />
@@ -21,7 +29,7 @@ const Hero = () => {
 							return (
 								<span
 									key={index}
-									className="letter-hover-effect cursor-pointer text-black  dark:text-white  "
+									className="letter-hover-effect cursor-pointer text-black   "
 								>
 									{letter}
 								</span>
@@ -31,7 +39,7 @@ const Hero = () => {
 					<p className=" text-[#8716f7] text-transparent  bg-clip-text bg-linear-to-l from-purple-500 to-blue-600 text-center text-3xl  text-wrap md:text-4xl mt-4   ">
 						Sergi López Hernández
 					</p>
-					<p className="font-[Arial] text-[1.7rem] p-5 text-black  text-center text-4xl mt-4 cursor-default">
+					<p className="font-[Arial] text-[1.5rem] md:text-[1.7rem] p-5 text-black  text-center text-4xl mt-4 cursor-default">
 						<TypeAnimation
 							sequence={[
 								"Web developer",
@@ -42,7 +50,7 @@ const Hero = () => {
 								3000,
 							]}
 							wrapper="span"
-							speed={55}
+							speed={60}
 							repeat={Infinity}
 						/>
 					</p>
@@ -52,14 +60,6 @@ const Hero = () => {
 					>
 						View Projects
 					</button>
-					<div className="flex justify-center items-center ">
-						<Image
-							src="/Logo_Gradient-removebg-preview.png"
-							alt="logo"
-							width={300}
-							height={300}
-						/>
-					</div>
 				</div>
 				<AboutMe />
 			</section>
